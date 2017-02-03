9ef443c45a0d6cc58e79e84b34c3b890a3baa6ad
Yao Yao <yyao@emperor-05.cs.wisc.edu>
2017-02-03 00:28:13 -0600

    first commit

---
+++ conf/lab.mk
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+LAB=6
+PACKAGEDATE=Tue Nov 8 15:45:36 EST 2016
+=======
+<<<<<<< HEAD
+LAB=5
+PACKAGEDATE=Thu Nov 3 09:15:49 EDT 2016
+=======
+<<<<<<< HEAD
+LAB=4
+PACKAGEDATE=Wed Oct 12 17:19:29 EDT 2016
+=======
+<<<<<<< HEAD
+LAB=3
+PACKAGEDATE=Thu Sep 29 14:25:51 EDT 2016
+=======
+<<<<<<< HEAD
+LAB=2
+PACKAGEDATE=Wed Sep 21 11:13:24 EDT 2016
+=======
+LAB=1
+PACKAGEDATE=Wed Sep 14 12:18:32 EDT 2016
+>>>>>>> lab1
+>>>>>>> lab2
+>>>>>>> lab3
+>>>>>>> lab4
+>>>>>>> lab5
